bcc06e2a7f3b6339159c923170f42445f4f3f476
AmirhosseinKashaf <kashafamirhossein@gmail.com>
2025-02-19 17:55:46 +0330

    forms done completely

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
 from website.models import Contact
 from website.forms import ContactForm,Newsletterform
+from django.contrib import messages
 # Create your views here.
 def index_view (request):
     return render(request,'website/index.html') 
@@ -14,6 +15,9 @@ def contact_view (request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submited succesfully.')
+        else:
+            messages.add_message(request,messages.ERROR,"your ticket didn't submit")
     form = ContactForm() 
     return render(request,'website/contact.html',{'form':form}) 
 
