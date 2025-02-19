043858a8653784a5714354ef63a206c14dd6d928
AmirhosseinKashaf <kashafamirhossein@gmail.com>
2025-02-19 14:21:46 +0330

    forms done
    done forms backend

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
+from website.models import Contact
+from website.forms import ContactForm,Newsletterform
 # Create your views here.
 def index_view (request):
     return render(request,'website/index.html') 
@@ -7,5 +9,19 @@ def index_view (request):
 def about_view (request):
     return render(request,'website/about.html') 
 
-def contact_view (request): 
-    return render(request,'website/contact.html') 
+def contact_view (request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm() 
+    return render(request,'website/contact.html',{'form':form}) 
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = Newsletterform(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
