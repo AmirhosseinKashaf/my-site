b70d2336135e3a13da8de8ae8a3a824952595299
AmirhosseinKashaf <kashafamirhossein@gmail.com>
2025-02-20 22:44:35 +0330

    sitemaps created

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -23,3 +25,6 @@ class Post(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.title,self.id)
+    
+    def get_absolute_url(self):
+        return reverse('blog:single',kwargs={'pid':self.id})
