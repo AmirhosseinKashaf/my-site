77c7ef6e21d8d013b2f8544368ecf1554ace2b24
AmirhosseinKashaf <kashafamirhossein@gmail.com>
2025-02-22 13:52:22 +0330

    taggit added

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -14,6 +14,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    tags = TaggableManager()
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',home_view,name='index'),
     path('<int:pid>',single_view,name='single'),
     path('category/<str:cat_name>',home_view,name='category'),
+    path('tag/<str:tag_name>',home_view,name='tag'),
     path('author/<str:author_username>',home_view,name='author'),
     path('search/',blog_search,name='search')
     # path('test',test,name='test'),
